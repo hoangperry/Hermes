e687a1f37e58f3c648338c42b240ab4d03573fed
hoangperry <prodai9573@gmail.com>
2019-12-31 11:28:32 +0700

    yaml to redis: set all key in once time

--- yaml_to_redis.py
+++ yaml_to_redis.py
@@ -4,9 +4,10 @@ import os
 import glob
 import argparse
 import json
-
+from crawler.application.common.crawler.environments import create_environments
 
 if __name__ == "__main__":
+    config = create_environments()
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--host", required=False, help="Redis server", default="localhost")
@@ -19,23 +20,25 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    redis_connect = redis.StrictRedis(host=args.host, port=args.port, db=args.db, password=args.password)
+    redis_connect = redis.StrictRedis(
+        host=config.redis_host, port=config.redis_port, db=config.redis_db, password=config.redis_password
+    )
 
     all_data = dict()
-    for yaml_file in glob.glob(os.path.join(args.yaml_folder, args.type, "pages/**.yaml")):
+    for _crawl_type in config.avaiable_crawl_type:
+        for yaml_file in glob.glob(os.path.join(config.yaml_folder, _crawl_type, "pages/**.yaml")):
+            with open(yaml_file, 'r') as stream:
+                yaml_data = yaml.safe_load(stream)
+                key = os.path.basename(os.path.splitext(yaml_file)[0])
+                # if yaml_data is None:
+                #     continue
+                all_data[key] = yaml_data
+
+        redis_connect.set(_crawl_type + "_rules", json.dumps(all_data))
+        yaml_file = os.path.join(config.yaml_folder, _crawl_type, "homepages.yaml")
+
         with open(yaml_file, 'r') as stream:
             yaml_data = yaml.safe_load(stream)
-            key = os.path.basename(os.path.splitext(yaml_file)[0])
-            # if yaml_data is None:
-            #     continue
-            all_data[key] = yaml_data
-
-    redis_connect.set(args.type + "_rules", json.dumps(all_data))
-
-    yaml_file = os.path.join(args.yaml_folder, args.type, "homepages.yaml")
-
-    with open(yaml_file, 'r') as stream:
-        yaml_data = yaml.safe_load(stream)
-        data = json.dumps(yaml_data)
-        print(args.type)
-        redis_connect.set(args.type + "_homes", data)
+            data = json.dumps(yaml_data)
+            print(_crawl_type)
+            redis_connect.set(_crawl_type + "_homes", data)
